e2bdded6cf7b7e675c761a6028fe1fc80d2a45b3
gody48 <109515290+gody48@users.noreply.github.com>
2022-08-05 16:31:14 +0530

    Update test.js

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ const { Builder, By, Key, until } = require("selenium-webdriver");
 (async function googleSearch() {
   let driver = await new Builder()
     .forBrowser("chrome")
-    .usingServer("http://localhost:4444/wd/hub/")
+    .usingServer("http://43.205.138.221:4444/wd/hub/")
     // .setChromeService(
     //  chrome.setDefaultService(
     //    new chrome.ServiceBuilder("./drivers/chromedriver_89").build()
